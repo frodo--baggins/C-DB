913e08387f4c61ca8cef61e2e75123c451ad39dc
frodo--baggins <enygard113@gmail.com>
2016-09-05 17:03:59 -0400

    connection.h modified to use MySQL Connector/C instead of MySQL Connector/C++

--- mysql/connection.h
+++ mysql/connection.h
@@ -1,32 +1,47 @@
 #ifndef CPPDB_MYSQL_CONNECTION_H
 #define CPPDB_MYSQL_CONNECTION_H
 
-#include <cppconn/driver.h>
-#include <cppconn/resultset.h>
-#include <cppconn/statement.h>
-#include <cppconn/connection.h>
+#include <my_global.h>
+#include <mysql.h>
 #include "../connection.h"
 #include "statement.h"
 #include "../exception.h"
 
 namespace cppdb {
 	class mysql_connection: public connection {
-		sql::Driver* driv;
-		sql::Connection* conn;
-		const char* dbname;
+		MYSQL* conn;
+		std::string dbname;
 	public:
-		mysql_connection(const std::string& url, const std::string& user, const std::string& pass = "")
-		: driv(get_driver_instance()), conn(driv->connect(url, user, pass)), dbname(NULL) {}
-		virtual ~mysql_connection() { delete conn; }
+		mysql_connection(const std::string& url, const std::string& u, const std::string& p):
+		conn(mysql_init(NULL)), dbname("") {
+			if(mysql_real_connect(conn, url, u, p, NULL, 0, NULL, 0) == NULL) {
+				std::string err = "Could not connect to database: ";
+				err += mysql_error(conn);
+				mysql_close(conn);
+				throw connect_error(err);
+			}
+		}
+
+		virtual ~mysql_connection() { mysql_close(conn); }
 		
-		virtual void select_database(const char* dbnme) { conn->setSchema(dbnme); dbname = dbnme; }
-		virtual void select_database(const std::string& dbnme) { return select_database(dbnme.c_str()); }
+		virtual void select_database(const char* dbnme) {
+			if(mysql_select_db(conn, dbnme) != 0) {
+				std::string err = "Could not select database: ";
+				err += mysql_error(conn);
+				throw connect_error(err);
+			}
+			dbname = dbnme;
+		}
+
+		virtual void select_database(const std::string& dbnme) {
+			return select_database(dbnme.c_str());
+		}
 		
 		virtual const char* database_name() const {
-			return dbname;		
+			return dbname;	
 		}
 		virtual statement* make_statement() {
-			return new mysql_statement(conn, conn->createStatement());
+			return new mysql_statement(conn);
 		}
 		//virtual prepared_statement* make_prepared_statement() = 0;
 	};
